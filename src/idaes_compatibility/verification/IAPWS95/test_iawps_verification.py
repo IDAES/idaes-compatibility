e7639c20d678eec64eadb661a480ab52397c5449
Andrew Lee <andrew.lee@netl.doe.gov>
2022-12-16 14:32:54 -0500

    Running black

--- src/idaes_compatibility/verification/IAPWS95/test_iawps_verification.py
+++ src/idaes_compatibility/verification/IAPWS95/test_iawps_verification.py
@@ -37,22 +37,36 @@ def test_iawps95_liquid_phase():
     m.fs.state[0].flow_mass.fix(1)
 
     for i, r in df.iterrows():
-        P = r['Pressure (MPa)']
+        P = r["Pressure (MPa)"]
         h = r["Enthalpy (kJ/kg)"]
         T = r["Temperature (K)"]
 
         if not (P == 22 and T > 646.85):  # Skip near the critical point
-            m.fs.state[0].pressure.fix(P*units.MPa)
-            m.fs.state[0].enth_mass.fix(h*units.kJ/units.kg)
+            m.fs.state[0].pressure.fix(P * units.MPa)
+            m.fs.state[0].enth_mass.fix(h * units.kJ / units.kg)
 
             assert value(m.fs.state[0].temperature) == pytest.approx(T, rel=1e-4)
-            assert value(m.fs.state[0].dens_mass_phase["Liq"]) == pytest.approx(r["Density (kg/m3)"], rel=1e-3)
-            assert value(m.fs.state[0].energy_internal_mass_phase["Liq"]) == pytest.approx(1e3*r["Internal Energy (kJ/kg)"], rel=1e-3)
-            assert value(m.fs.state[0].entr_mass_phase["Liq"]) == pytest.approx(1e3*r["Entropy (J/g*K)"], rel=1e-2)
-            assert value(m.fs.state[0].cv_mass_phase["Liq"]) == pytest.approx(1e3*r["Cv (J/g*K)"], rel=2e-2)
-            assert value(m.fs.state[0].cp_mass_phase["Liq"]) == pytest.approx(1e3*r["Cp (J/g*K)"], rel=2e-2)
-            assert value(m.fs.state[0].speed_sound_phase["Liq"]) == pytest.approx(r["Sound Spd. (m/s)"], rel=1e-3)
-            assert value(m.fs.state[0].visc_d_phase["Liq"]) == pytest.approx(r["Viscosity (Pa*s)"], rel=2e-2)
+            assert value(m.fs.state[0].dens_mass_phase["Liq"]) == pytest.approx(
+                r["Density (kg/m3)"], rel=1e-3
+            )
+            assert value(
+                m.fs.state[0].energy_internal_mass_phase["Liq"]
+            ) == pytest.approx(1e3 * r["Internal Energy (kJ/kg)"], rel=1e-3)
+            assert value(m.fs.state[0].entr_mass_phase["Liq"]) == pytest.approx(
+                1e3 * r["Entropy (J/g*K)"], rel=1e-2
+            )
+            assert value(m.fs.state[0].cv_mass_phase["Liq"]) == pytest.approx(
+                1e3 * r["Cv (J/g*K)"], rel=2e-2
+            )
+            assert value(m.fs.state[0].cp_mass_phase["Liq"]) == pytest.approx(
+                1e3 * r["Cp (J/g*K)"], rel=2e-2
+            )
+            assert value(m.fs.state[0].speed_sound_phase["Liq"]) == pytest.approx(
+                r["Sound Spd. (m/s)"], rel=1e-3
+            )
+            assert value(m.fs.state[0].visc_d_phase["Liq"]) == pytest.approx(
+                r["Viscosity (Pa*s)"], rel=2e-2
+            )
             # Thermal conductivity does not match to less than 5% error.
 
 
@@ -71,22 +85,36 @@ def test_iawps95_vapor_phase():
     m.fs.state[0].flow_mass.fix(1)
 
     for i, r in df.iterrows():
-        P = r['Pressure (MPa)']
+        P = r["Pressure (MPa)"]
         h = r["Enthalpy (kJ/kg)"]
         T = r["Temperature (K)"]
 
         if not (P == 22 and T < 647):  # Skip near the critical point
-            m.fs.state[0].pressure.fix(P*units.MPa)
-            m.fs.state[0].enth_mass.fix(h*units.kJ/units.kg)
+            m.fs.state[0].pressure.fix(P * units.MPa)
+            m.fs.state[0].enth_mass.fix(h * units.kJ / units.kg)
 
             assert value(m.fs.state[0].temperature) == pytest.approx(T, rel=1e-4)
-            assert value(m.fs.state[0].dens_mass_phase["Vap"]) == pytest.approx(r["Density (kg/m3)"], rel=5e-3)
-            assert value(m.fs.state[0].energy_internal_mass_phase["Vap"]) == pytest.approx(1e3*r["Internal Energy (kJ/kg)"], rel=1e-3)
-            assert value(m.fs.state[0].entr_mass_phase["Vap"]) == pytest.approx(1e3*r["Entropy (J/g*K)"], rel=1e-2)
-            assert value(m.fs.state[0].cv_mass_phase["Vap"]) == pytest.approx(1e3*r["Cv (J/g*K)"], rel=2e-2)
-            assert value(m.fs.state[0].cp_mass_phase["Vap"]) == pytest.approx(1e3*r["Cp (J/g*K)"], rel=2e-2)
-            assert value(m.fs.state[0].speed_sound_phase["Vap"]) == pytest.approx(r["Sound Spd. (m/s)"], rel=1e-3)
-            assert value(m.fs.state[0].visc_d_phase["Vap"]) == pytest.approx(r["Viscosity (Pa*s)"], rel=3e-2)
+            assert value(m.fs.state[0].dens_mass_phase["Vap"]) == pytest.approx(
+                r["Density (kg/m3)"], rel=5e-3
+            )
+            assert value(
+                m.fs.state[0].energy_internal_mass_phase["Vap"]
+            ) == pytest.approx(1e3 * r["Internal Energy (kJ/kg)"], rel=1e-3)
+            assert value(m.fs.state[0].entr_mass_phase["Vap"]) == pytest.approx(
+                1e3 * r["Entropy (J/g*K)"], rel=1e-2
+            )
+            assert value(m.fs.state[0].cv_mass_phase["Vap"]) == pytest.approx(
+                1e3 * r["Cv (J/g*K)"], rel=2e-2
+            )
+            assert value(m.fs.state[0].cp_mass_phase["Vap"]) == pytest.approx(
+                1e3 * r["Cp (J/g*K)"], rel=2e-2
+            )
+            assert value(m.fs.state[0].speed_sound_phase["Vap"]) == pytest.approx(
+                r["Sound Spd. (m/s)"], rel=1e-3
+            )
+            assert value(m.fs.state[0].visc_d_phase["Vap"]) == pytest.approx(
+                r["Viscosity (Pa*s)"], rel=3e-2
+            )
             # Thermal conductivity does not match to less than 5% error.
 
 
@@ -105,21 +133,35 @@ def test_iawps95_supercritical():
     m.fs.state[0].flow_mass.fix(1)
 
     for i, r in df.iterrows():
-        P = r['Pressure (MPa)']
+        P = r["Pressure (MPa)"]
         h = r["Enthalpy (kJ/kg)"]
         T = r["Temperature (K)"]
 
-        m.fs.state[0].pressure.fix(P*units.MPa)
-        m.fs.state[0].enth_mass.fix(h*units.kJ/units.kg)
+        m.fs.state[0].pressure.fix(P * units.MPa)
+        m.fs.state[0].enth_mass.fix(h * units.kJ / units.kg)
 
         assert value(m.fs.state[0].temperature) == pytest.approx(T, rel=1e-4)
-        assert value(m.fs.state[0].dens_mass) == pytest.approx(r["Density (kg/m3)"], rel=5e-3)
-        assert value(m.fs.state[0].energy_internal_mass) == pytest.approx(1e3*r["Internal Energy (kJ/kg)"], rel=1e-3)
-        assert value(m.fs.state[0].entr_mass) == pytest.approx(1e3*r["Entropy (J/g*K)"], rel=1e-2)
-        assert value(m.fs.state[0].cv_mass) == pytest.approx(1e3*r["Cv (J/g*K)"], rel=2e-2)
-        assert value(m.fs.state[0].cp_mass) == pytest.approx(1e3*r["Cp (J/g*K)"], rel=5e-2)
-        assert value(m.fs.state[0].speed_sound_phase["Vap"]) == pytest.approx(r["Sound Spd. (m/s)"], rel=1e-2)
-        assert value(m.fs.state[0].visc_d_phase["Vap"]) == pytest.approx(r["Viscosity (Pa*s)"], rel=5e-2)
+        assert value(m.fs.state[0].dens_mass) == pytest.approx(
+            r["Density (kg/m3)"], rel=5e-3
+        )
+        assert value(m.fs.state[0].energy_internal_mass) == pytest.approx(
+            1e3 * r["Internal Energy (kJ/kg)"], rel=1e-3
+        )
+        assert value(m.fs.state[0].entr_mass) == pytest.approx(
+            1e3 * r["Entropy (J/g*K)"], rel=1e-2
+        )
+        assert value(m.fs.state[0].cv_mass) == pytest.approx(
+            1e3 * r["Cv (J/g*K)"], rel=2e-2
+        )
+        assert value(m.fs.state[0].cp_mass) == pytest.approx(
+            1e3 * r["Cp (J/g*K)"], rel=5e-2
+        )
+        assert value(m.fs.state[0].speed_sound_phase["Vap"]) == pytest.approx(
+            r["Sound Spd. (m/s)"], rel=1e-2
+        )
+        assert value(m.fs.state[0].visc_d_phase["Vap"]) == pytest.approx(
+            r["Viscosity (Pa*s)"], rel=5e-2
+        )
         # Thermal conductivity does not match to less than 5% error.
 
 
@@ -138,7 +180,7 @@ def test_iawps95_saturated():
     m.fs.state[0].flow_mass.fix(1)
 
     for i, r in df.iterrows():
-        P = r['Pressure (MPa)']
+        P = r["Pressure (MPa)"]
         h = r["Enthalpy (kJ/kg)"]
         T = r["Temperature (K)"]
 
@@ -147,16 +189,32 @@ def test_iawps95_saturated():
         else:
             phase = "Vap"
 
-        if not (i == 0 or (P == 21.899  and T == 646.47)):  # Skip first point & near critical point
-            m.fs.state[0].pressure.fix(P*units.MPa)
-            m.fs.state[0].enth_mass.fix(h*units.kJ/units.kg)
+        if not (
+            i == 0 or (P == 21.899 and T == 646.47)
+        ):  # Skip first point & near critical point
+            m.fs.state[0].pressure.fix(P * units.MPa)
+            m.fs.state[0].enth_mass.fix(h * units.kJ / units.kg)
 
             assert value(m.fs.state[0].temperature) == pytest.approx(T, rel=1e-4)
-            assert value(m.fs.state[0].dens_mass_phase[phase]) == pytest.approx(r["Density (kg/m3)"], rel=5e-3)
-            assert value(m.fs.state[0].energy_internal_mass_phase[phase]) == pytest.approx(1e3*r["Internal Energy (kJ/kg)"], rel=1e-3)
-            assert value(m.fs.state[0].entr_mass_phase[phase]) == pytest.approx(1e3*r["Entropy (J/g*K)"], rel=1e-2)
-            assert value(m.fs.state[0].cv_mass_phase[phase]) == pytest.approx(1e3*r["Cv (J/g*K)"], rel=2e-2)
-            assert value(m.fs.state[0].cp_mass_phase[phase]) == pytest.approx(1e3*r["Cp (J/g*K)"], rel=2e-2)
-            assert value(m.fs.state[0].speed_sound_phase[phase]) == pytest.approx(r["Sound Spd. (m/s)"], rel=5e-3)
-            assert value(m.fs.state[0].visc_d_phase[phase]) == pytest.approx(r["Viscosity (Pa*s)"], rel=3e-2)
+            assert value(m.fs.state[0].dens_mass_phase[phase]) == pytest.approx(
+                r["Density (kg/m3)"], rel=5e-3
+            )
+            assert value(
+                m.fs.state[0].energy_internal_mass_phase[phase]
+            ) == pytest.approx(1e3 * r["Internal Energy (kJ/kg)"], rel=1e-3)
+            assert value(m.fs.state[0].entr_mass_phase[phase]) == pytest.approx(
+                1e3 * r["Entropy (J/g*K)"], rel=1e-2
+            )
+            assert value(m.fs.state[0].cv_mass_phase[phase]) == pytest.approx(
+                1e3 * r["Cv (J/g*K)"], rel=2e-2
+            )
+            assert value(m.fs.state[0].cp_mass_phase[phase]) == pytest.approx(
+                1e3 * r["Cp (J/g*K)"], rel=2e-2
+            )
+            assert value(m.fs.state[0].speed_sound_phase[phase]) == pytest.approx(
+                r["Sound Spd. (m/s)"], rel=5e-3
+            )
+            assert value(m.fs.state[0].visc_d_phase[phase]) == pytest.approx(
+                r["Viscosity (Pa*s)"], rel=3e-2
+            )
             # Thermal conductivity does not match to less than 5% error.
