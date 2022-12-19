0d04e58698169858d6aa8ea32dcc8bc5295f43c9
Andrew Lee <andrew.lee@netl.doe.gov>
2022-12-19 11:20:59 -0500

    Adding ALAMO example test

--- src/idaes_compatibility/apis/v2_0_0_a3/examples/surrogates/test_alamopy_example.py
+++ src/idaes_compatibility/apis/v2_0_0_a3/examples/surrogates/test_alamopy_example.py
@@ -11,7 +11,6 @@
 # at the URL "https://github.com/IDAES/idaes-pse".
 ##############################################################################
 import pytest
-
 import os
 import numpy as np
 import pandas as pd
@@ -31,7 +30,7 @@ from pyomo.environ import (
 from pyomo.common.timing import TicTocTimer
 
 from idaes.core.surrogate.sampling.data_utils import split_training_validation
-from idaes.core.surrogate.pysmo_surrogate import PysmoPolyTrainer, PysmoSurrogate
+from idaes.core.surrogate.alamopy import AlamoTrainer, AlamoSurrogate, alamo
 from idaes.core.surrogate.plotting.sm_plotter import (
     surrogate_scatter2D,
     surrogate_parity,
@@ -50,54 +49,69 @@ path = os.path.dirname(os.path.abspath(__file__))
 data_file = os.path.join(path, "reformer-data.csv")
 
 
+@pytest.mark.skipif(not alamo.available(), reason="ALAMO not available")
 @patch("matplotlib.pyplot.show")
 def test_example(mock_show):
     np.set_printoptions(precision=6, suppress=True)
 
     csv_data = pd.read_csv(data_file)  # 2800 data points
     data = csv_data.sample(n=100)  # randomly sample points for training/validation
+
     input_data = data.iloc[:, :2]
     output_data = data.iloc[:, 2:]
 
-    input_labels = list(input_data.columns)
-    output_labels = list(output_data.columns)
+    # Define labels, and split training and validation data
+    input_labels = input_data.columns
+    output_labels = output_data.columns
 
     n_data = data[input_labels[0]].size
     data_training, data_validation = split_training_validation(
         data, 0.8, seed=n_data
     )  # seed=100
 
+    # capture long output (not required to use surrogate API)
     stream = StringIO()
     oldstdout = sys.stdout
     sys.stdout = stream
 
-    # Create PySMO trainer object
-    trainer = PysmoPolyTrainer(
-        input_labels=input_labels,
-        output_labels=output_labels,
-        training_dataframe=data_training,
-    )
-
-    # Set PySMO options
-    trainer.config.maximum_polynomial_order = 6
-    trainer.config.multinomials = True
-    trainer.config.training_split = 0.8
-    trainer.config.number_of_crossvalidations = 10
-
-    # Train surrogate (calls PySMO through IDAES Python wrapper)
-    poly_train = trainer.train_surrogate()
-
-    # create callable surrogate object
-    xmin, xmax = [0.1, 0.8], [0.8, 1.2]
-    input_bounds = {
-        input_labels[i]: (xmin[i], xmax[i]) for i in range(len(input_labels))
-    }
-    poly_surr = PysmoSurrogate(poly_train, input_labels, output_labels, input_bounds)
-
     with TempfileManager as tf:
+        # Create ALAMO trainer object
+        trainer = AlamoTrainer(
+            input_labels=input_labels,
+            output_labels=output_labels,
+            training_dataframe=data_training,
+        )
+
+        # Set ALAMO options
+        trainer.config.constant = True
+        trainer.config.linfcns = True
+        trainer.config.multi2power = [1, 2]
+        trainer.config.monomialpower = [2, 3]
+        trainer.config.ratiopower = [1, 2]
+        trainer.config.maxterms = [10] * len(output_labels)  # max for each surrogate
+        tmpalm = tf.create_tempfile(suffix=".alm")
+        trainer.config.filename = os.path.join(tmpalm)
+        trainer.config.overwrite_files = True
+
+        # Train surrogate (calls ALAMO through IDAES ALAMOPy wrapper)
+        success, alm_surr, msg = trainer.train_surrogate()
+
         # save model to JSON
         tmpjson = tf.create_tempfile(suffix=".json")
-        model = poly_surr.save_to_file(tmpjson, overwrite=True)
+        model = alm_surr.save_to_file(tmpjson, overwrite=True)
+
+        # create callable surrogate object
+        surrogate_expressions = trainer._results["Model"]
+        input_labels = trainer._input_labels
+        output_labels = trainer._output_labels
+        xmin, xmax = [0.1, 0.8], [0.8, 1.2]
+        input_bounds = {
+            input_labels[i]: (xmin[i], xmax[i]) for i in range(len(input_labels))
+        }
+
+        alm_surr = AlamoSurrogate(
+            surrogate_expressions, input_labels, output_labels, input_bounds
+        )
 
         # revert back to normal output capture
         sys.stdout = oldstdout
@@ -114,76 +128,70 @@ def test_example(mock_show):
 
         # visualize with IDAES surrogate plotting tools
         fname = tf.create_tempfile(suffix=".pdf")
-        surrogate_scatter2D(poly_surr, data_training, filename=fname)
+        surrogate_scatter2D(alm_surr, data_training, filename=fname)
         fname = tf.create_tempfile(suffix=".pdf")
-        surrogate_parity(poly_surr, data_training, filename=fname)
+        surrogate_parity(alm_surr, data_training, filename=fname)
         fname = tf.create_tempfile(suffix=".pdf")
-        surrogate_residual(poly_surr, data_training, filename=fname)
+        surrogate_residual(alm_surr, data_training, filename=fname)
 
         # visualize with IDAES surrogate plotting tools
         fname = tf.create_tempfile(suffix=".pdf")
-        surrogate_scatter2D(poly_surr, data_validation, filename=fname)
+        surrogate_scatter2D(alm_surr, data_validation, filename=fname)
         fname = tf.create_tempfile(suffix=".pdf")
-        surrogate_parity(poly_surr, data_validation, filename=fname)
+        surrogate_parity(alm_surr, data_validation, filename=fname)
         fname = tf.create_tempfile(suffix=".pdf")
-        surrogate_residual(poly_surr, data_validation, filename=fname)
+        surrogate_residual(alm_surr, data_validation, filename=fname)
 
-        # create the IDAES model and flowsheet
-        m = ConcreteModel()
-        m.fs = FlowsheetBlock(dynamic=False)
+    # create the IDAES model and flowsheet
+    m = ConcreteModel()
+    m.fs = FlowsheetBlock(dynamic=False)
 
-        # create flowsheet input variables
-        m.fs.bypass_frac = Var(
-            initialize=0.80, bounds=[0.1, 0.8], doc="natural gas bypass fraction"
-        )
-        m.fs.ng_steam_ratio = Var(
-            initialize=0.80, bounds=[0.8, 1.2], doc="natural gas to steam ratio"
-        )
+    # create flowsheet input variables
+    m.fs.bypass_frac = Var(
+        initialize=0.80, bounds=[0.1, 0.8], doc="natural gas bypass fraction"
+    )
+    m.fs.ng_steam_ratio = Var(
+        initialize=0.80, bounds=[0.8, 1.2], doc="natural gas to steam ratio"
+    )
 
-        # create flowsheet output variables
-        m.fs.steam_flowrate = Var(initialize=0.2, doc="steam flowrate")
-        m.fs.reformer_duty = Var(initialize=10000, doc="reformer heat duty")
-        m.fs.AR = Var(initialize=0, doc="AR fraction")
-        m.fs.C2H6 = Var(initialize=0, doc="C2H6 fraction")
-        m.fs.C3H8 = Var(initialize=0, doc="C3H8 fraction")
-        m.fs.C4H10 = Var(initialize=0, doc="C4H10 fraction")
-        m.fs.CH4 = Var(initialize=0, doc="CH4 fraction")
-        m.fs.CO = Var(initialize=0, doc="CO fraction")
-        m.fs.CO2 = Var(initialize=0, doc="CO2 fraction")
-        m.fs.H2 = Var(initialize=0, doc="H2 fraction")
-        m.fs.H2O = Var(initialize=0, doc="H2O fraction")
-        m.fs.N2 = Var(initialize=0, doc="N2 fraction")
-        m.fs.O2 = Var(initialize=0, doc="O2 fraction")
+    # create flowsheet output variables
+    m.fs.steam_flowrate = Var(initialize=0.2, doc="steam flowrate")
+    m.fs.reformer_duty = Var(initialize=10000, doc="reformer heat duty")
+    m.fs.AR = Var(initialize=0, doc="AR fraction")
+    m.fs.C2H6 = Var(initialize=0, doc="C2H6 fraction")
+    m.fs.C3H8 = Var(initialize=0, doc="C3H8 fraction")
+    m.fs.C4H10 = Var(initialize=0, doc="C4H10 fraction")
+    m.fs.CH4 = Var(initialize=0, doc="CH4 fraction")
+    m.fs.CO = Var(initialize=0, doc="CO fraction")
+    m.fs.CO2 = Var(initialize=0, doc="CO2 fraction")
+    m.fs.H2 = Var(initialize=0, doc="H2 fraction")
+    m.fs.H2O = Var(initialize=0, doc="H2O fraction")
+    m.fs.N2 = Var(initialize=0, doc="N2 fraction")
+    m.fs.O2 = Var(initialize=0, doc="O2 fraction")
 
-        # create input and output variable object lists for flowsheet
-        inputs = [m.fs.bypass_frac, m.fs.ng_steam_ratio]
-        outputs = [
-            m.fs.steam_flowrate,
-            m.fs.reformer_duty,
-            m.fs.AR,
-            m.fs.C2H6,
-            m.fs.C4H10,
-            m.fs.C3H8,
-            m.fs.CH4,
-            m.fs.CO,
-            m.fs.CO2,
-            m.fs.H2,
-            m.fs.H2O,
-            m.fs.N2,
-            m.fs.O2,
-        ]
+    # create input and output variable object lists for flowsheet
+    inputs = [m.fs.bypass_frac, m.fs.ng_steam_ratio]
+    outputs = [
+        m.fs.steam_flowrate,
+        m.fs.reformer_duty,
+        m.fs.AR,
+        m.fs.C2H6,
+        m.fs.C4H10,
+        m.fs.C3H8,
+        m.fs.CH4,
+        m.fs.CO,
+        m.fs.CO2,
+        m.fs.H2,
+        m.fs.H2O,
+        m.fs.N2,
+        m.fs.O2,
+    ]
 
-        # capture long output (not required to use surrogate API)
-        stream = StringIO()
-        oldstdout = sys.stdout
-        sys.stdout = stream
-
-        surrogate = PysmoSurrogate.load_from_file(tmpjson)
-        m.fs.surrogate = SurrogateBlock(concrete=True)
-        m.fs.surrogate.build_model(surrogate, input_vars=inputs, output_vars=outputs)
-
-    # revert back to normal output capture - don't need to print PySMO load output
-    sys.stdout = oldstdout
+    # create the Pyomo/IDAES block that corresponds to the surrogate
+    # ALAMO
+    surrogate = AlamoSurrogate.load_from_file("alamo_surrogate.json")
+    m.fs.surrogate = SurrogateBlock()
+    m.fs.surrogate.build_model(surrogate, input_vars=inputs, output_vars=outputs)
 
     # fix input values and solve flowsheet
     m.fs.bypass_frac.fix(0.5)
